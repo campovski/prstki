f1e7eb0cb16b0301890741e7020d0126e0161316
SlivnikN <nina.slivnik@gmail.com>
2016-03-29 10:19:35 +0200

    Popravi delitev - se ne dela

--- drevo_igre.py
+++ drevo_igre.py
@@ -5,10 +5,10 @@ mnozica_potez = set()
 
 #### POPRAVI: narisi_drevo(4)??
 
-def narisi_drevo(globina, prsti, roke, visina=0):
+def narisi_drevo(globina, visina=0):
 	""" Funkcija, ki nariše drevo igre za igralca s poljubnim številom rok in prstov. V drevesu so predstavljene vse možne poteze na vsakem koraku. """
 	zacetno_vozlisce = ""
-	igralec_na_potezi = str(x.na_potezi)
+	igralec_na_potezi = str(x.na_potezi + 1)
 	for j, igralec in enumerate(x.position):
 		# Zapiši zacetno_vozlisce v string
 		for i, roka in enumerate(igralec):
@@ -18,10 +18,15 @@ def narisi_drevo(globina, prsti, roke, visina=0):
 				zacetno_vozlisce += str(roka) + "  "
 			else:
 				zacetno_vozlisce += str(roka)
-	for elt in x.veljavne_poteze():
-		if elt[0]:
-			x.opravi_delitev()
-			x.opravi_potezo(elt[1],elt[2])
+	if x.je_veljavna_delitev():
+		x.opravi_delitev()
+		poteze_po_delitvi_1 = x.veljavne_poteze()
+		x.razveljavi_potezo()
+		seznam_vseh_potez_1 = x.veljavne_poteze() + poteze_po_delitvi_1
+	else: seznam_vseh_potez_1 = x.veljavne_poteze()
+	for (je_mozna_delitev, roka_napadalca, roka_napadenca) in seznam_vseh_potez_1:
+		if x.opravi_potezo != x.je_konec():
+			x.opravi_potezo(roka_napadalca,roka_napadenca)
 			koncno_vozlisce = ""
 			for j, igralec in enumerate(x.position):
 				# Zapiši koncno_vozlisce v string
@@ -33,39 +38,20 @@ def narisi_drevo(globina, prsti, roke, visina=0):
 					else:
 						koncno_vozlisce += str(roka)
 			mnozica_potez.add((str(visina), str(visina+1), igralec_na_potezi, zacetno_vozlisce, koncno_vozlisce))
-			x.razveljavi_potezo()
-		else:
-			print("Bedno")
-			x.opravi_potezo(elt[1],elt[2])
-			koncno_vozlisce = ""
-			for j, igralec in enumerate(x.position):
-				print(x.position)
-				# Zapiši koncno_vozlisce v string
-				for i, roka in enumerate(igralec):
-					if i < len(igralec)-1:
-						koncno_vozlisce += str(roka) + ","
-					elif j == 0:
-						koncno_vozlisce += str(roka) + "  "
-					else:
-						koncno_vozlisce += str(roka)
-			mnozica_potez.add((str(visina), str(visina+1), igralec_na_potezi, zacetno_vozlisce, koncno_vozlisce))
-			x.razveljavi_potezo()
+		x.razveljavi_potezo()
 	if globina > 0:
-		for elt in x.veljavne_poteze():
-			if elt[0]:
-				x.opravi_delitev()
-				x.opravi_potezo(elt[1],elt[2])
+		if x.je_veljavna_delitev():
+			x.opravi_delitev()
+			poteze_po_delitvi_2 = x.veljavne_poteze()
+			x.razveljavi_potezo()
+			seznam_vseh_potez_2 = x.veljavne_poteze() + poteze_po_delitvi_2
+		else: seznam_vseh_potez_2 = x.veljavne_poteze()
+		for (je_mozna_delitev, roka_napadalca, roka_napadenca) in seznam_vseh_potez_2:
+			if x.opravi_potezo != x.je_konec():
+				x.opravi_potezo(roka_napadalca,roka_napadenca)
 				if x.je_konec(): pass
 				else:
-					narisi_drevo(globina-1, prsti, roke, visina+1)
-				x.razveljavi_potezo()
-			else:
-				print("Blabla")
-				x.opravi_potezo(elt[1],elt[2])
-				if x.je_konec(): pass
-				else:
-					print("Ne dela!")
-					narisi_drevo(globina-1, prsti, roke, visina+1)
+					narisi_drevo(globina-1, visina+1)
 				x.razveljavi_potezo()	
 				
 
@@ -73,7 +59,7 @@ def naredi_dot_drevo(globina, prsti=5, roke=2):
 	''' Funkcija elemente množice mnozica_potez zapiše v datoteko drevo_igre.dot. '''
 	try: os.remove("drevo_igre.dot")
 	except: pass
-	narisi_drevo(globina, prsti, roke)
+	narisi_drevo(globina)
 	izhod = open("drevo_igre.dot", "w")
 	izhod.write("digraph {\n       node [style = filled];\n")
 	konec1 = re.compile('0,0  \d,\d')
